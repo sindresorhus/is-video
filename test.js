d4f578ddbb64aa43bc140bcc0cbf8251a35bd876
Sindre Sorhus <sindresorhus@gmail.com>
2015-05-25 13:29:58 +0200

    minor tweaks

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ var isVideo = require('./');
 test(function (t) {
 	t.assert(isVideo('unicorn.mp4'));
 	t.assert(isVideo('unicorn.mkv'));
+	t.assert(isVideo('unicorn.MKV'));
 	t.assert(!isVideo('unicorn.jpg'));
 	t.assert(!isVideo('unicorn.txt'));
 	t.assert(!isVideo('unicornzip'));
